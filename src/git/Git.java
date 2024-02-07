297bfff17cbdd97570caa03c530dcc9ac0755906
Asus <Asus@192.168.0.113>
2024-02-07 15:06:03 +0800

    version 2. 1.1

--- src/git/Git.java
+++ src/git/Git.java
@@ -14,7 +14,10 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hello World");
+        int a = 1;
+        System.out.println();
     }
     
 }
